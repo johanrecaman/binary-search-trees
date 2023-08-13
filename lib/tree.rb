1325f4782bf9e8ce9f2fe698e22d269ab1beae83
Johan Stromberg <johanstrr@gmail.com>
2023-08-13 20:25:53 -0300

    insert node function added

--- lib/tree.rb
+++ lib/tree.rb
@@ -12,18 +12,31 @@ class Tree
         end
 
         mid = arr.length / 2
-        root = Node.new(arr[mid], nil, nil)
-        root.left = build_tree(arr[0...mid])
-        root.right = build_tree(arr[mid + 1..-1])
+        root = Node.new(arr[mid], build_tree(arr[0...mid]), build_tree(arr[mid + 1..-1]))
+        return root
+   end
+
+   def insert(value)
+        @root = insert_node(value, @root)
+   end
+
+   def insert_node(value, root)
+        if root.nil?
+            return Node.new(value, nil, nil)
+        end
+
+        if value < root.data
+            root.left = insert_node(value, root.left)
+        elsif value > root.data
+            root.right = insert_node(value, root.right)
+        end
         return root
     end
-
+    
     def pretty_print(node = @root, prefix = '', is_left = true)
         pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
         pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-      end
-      
+    end
+
 end
-
-
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative 'lib/tree'
 
 tree = Tree.new([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+tree.insert(15)
 tree.pretty_print
